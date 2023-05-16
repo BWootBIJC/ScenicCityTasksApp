d336c375f3bbc26a76c33eb0e22fd5eaf2a37089
Bryant Wooters <Jesusrox45@gmail.com>
2023-05-16 19:08:00 -0400

    Install project dependencies for frontend and backend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [
+          require('tailwindcss'),
+          require('autoprefixer')
+      ]
+    }
+  }
 })
