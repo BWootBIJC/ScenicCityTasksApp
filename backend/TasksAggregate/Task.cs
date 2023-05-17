67eeba4ca7a926b6432e73aba12b1636e2f8797e
Bryant Wooters <Jesusrox45@gmail.com>
2023-05-16 20:45:37 -0400

    Update task entity model

--- backend/TasksAggregate/Task.cs
+++ backend/TasksAggregate/Task.cs
@@ -6,7 +6,7 @@ public class Task
 {
     public int Id { get; set; }
     public string Name { get; set; }
-    public string Description { get; set; }
+    public string? Description { get; set; }
 
     public Task(int id, string name, string description)
     {
--- backend/TasksAggregate/ViewModels/TaskEditViewModel.cs
+++ backend/TasksAggregate/ViewModels/TaskEditViewModel.cs
@@ -4,9 +4,9 @@ public class TaskEditViewModel
 {
     public int Id { get; set; }
     public string Name { get; set; }
-    public string Description { get; set; }
+    public string? Description { get; set; }
 
-    public TaskEditViewModel(int id, string name, string description)
+    public TaskEditViewModel(int id, string name, string? description)
     {
         Id = id;
         Name = name;
