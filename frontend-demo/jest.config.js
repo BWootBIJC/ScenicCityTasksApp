816dc828e68098a9e55fb0dc89bef1eeb4d17a38
Bryant <jesusrox45@gmail.com>
2023-06-01 23:19:15 -0400

    Fix test setup

--- frontend-demo/jest.config.js
+++ frontend-demo/jest.config.js
@@ -1,12 +1,12 @@
 ﻿export default {
     transform: {
-        '^.+\\.[jt]sx?$': 'babel-jest',
+        '^.+\\.[jt]sx?$': 'ts-jest',
     },
     setupFilesAfterEnv: ['./jest.setup.js'],
     testEnvironment: 'jest-environment-jsdom',
     testMatch: ['**/__tests__/**/*.ts?(x)', '**/?(*.)+(spec|test).ts?(x)'],
     globals: {
-        'babel-jest': {
+        'ts-jest': {
             diagnostics: false,
             isolatedModules: true
         },
