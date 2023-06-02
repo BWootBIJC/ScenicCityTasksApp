3a924a928aa99133c0ccf15fffb7328f63d4f5f9
Bryant <jesusrox45@gmail.com>
2023-06-02 00:36:23 -0400

    Remove deleted component references

--- frontend-demo/src/App.tsx
+++ frontend-demo/src/App.tsx
@@ -1,20 +1,9 @@
 import {Home} from "./pages/Home";
-import {APIGateway} from "./apiGateway/APIGateway";
-import {APIResponseHandler} from "./apiGateway/APIResponseHandler";
-import {TaskGateway} from "./taskAggregate/gateway/TaskGateway";
-import {TaskRepository} from "./taskAggregate/repository/TaskRepository";
-import {TaskContextProvider} from "./taskAggregate/state/TaskContext";
-
 function App() {
-    const apiHandler = new APIResponseHandler();
-    const apiGateway = new APIGateway(apiHandler);
-    const taskGateway = new TaskGateway(apiGateway);
 
     return (
         <>
-            <TaskContextProvider taskRepo={new TaskRepository(taskGateway)}>
-                <Home/>
-            </TaskContextProvider>
+            <Home/>
         </>
     )
 }
