afae2a199b27188045b16a0cf0f52347bde88d99
Bryant <jesusrox45@gmail.com>
2023-06-02 00:33:54 -0400

    Remove demo files for presentation

--- frontend-demo/src/taskAggregate/gateway/ITaskGateway.ts
+++
@@ -1,8 +0,0 @@
-﻿import {TaskListViewModel} from "../viewModels/TaskListViewModel";
-import {TaskCreateViewModel} from "../viewModels/TaskCreateViewModel";
-
-export interface ITaskGateway {
-    GetAllTasks: () => Promise<TaskListViewModel[]>;
-    AddTask: (taskCreateVm: TaskCreateViewModel) => Promise<number>;
-    DeleteTask: (taskId: number) => Promise<void>;
-}
